a000c343e9385456cf6f9ba7ac3c46f1f7508fdd
Fabricio Pedreira <fabriciopedreira@gmail.com>
2017-03-12 11:53:20 -0300

    corrigindo lista

--- app.js
+++ app.js
@@ -601,58 +601,79 @@ function sendButtonContaFacilMessage(recipientId) {
 }
 
 function sendListContaFacilMessage(recipientId) {
-	  var messageData = {
-	    recipient: {
-	      id: recipientId
-	    },
-	    message: {
-	      attachment: {
-	        type: "template",
-	        payload: {
-	            template_type: "list",
-	            elements: [{
-                    title: "Classic T-Shirt Collection",
-                    image_url: "https://peterssendreceiveapp.ngrok.io/img/collection.png",
-                    subtitle: "See all our colors",
-                    default_action: {
-                        "type": "web_url",
-                        "url": "https://peterssendreceiveapp.ngrok.io/shop_collection",
-                        "messenger_extensions": true,
-                        "webview_height_ratio": "tall",
-                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                    },
-                    buttons: [{
-                        title: "View",
-                        type: "web_url",
-                        url: "https://peterssendreceiveapp.ngrok.io/collection",
-                        messenger_extensions: "true",
-                        webview_height_ratio: "tall",
-                        fallback_url: "https://peterssendreceiveapp.ngrok.io/"                        
-                    }]
-	            }]
-	        }
-	      }
-	    }
-	  };  
+	var messageData = {
+		recipient : {
+			id : recipientId
+		},
+		message : {
+			attachment : {
+				type : "template",
+				payload : {
+					template_type : "list",
+					elements : [
+							{
+								title : "Classic T-Shirt Collection",
+								image_url : "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+								subtitle : "See all our colors",
+								default_action : {
+									"type" : "web_url",
+									"url" : "https://peterssendreceiveapp.ngrok.io/shop_collection",
+									"messenger_extensions" : true,
+									"webview_height_ratio" : "tall",
+									"fallback_url" : "https://peterssendreceiveapp.ngrok.io/"
+								},
+								buttons : [ {
+									title : "View",
+									type : "web_url",
+									url : "https://peterssendreceiveapp.ngrok.io/collection",
+									messenger_extensions : "true",
+									webview_height_ratio : "tall",
+									fallback_url : "https://peterssendreceiveapp.ngrok.io/"
+								} ]
+							},
+							{
+								title : "Classic White T-Shirt",
+								image_url : "https://peterssendreceiveapp.ngrok.io/img/white-t-shirt.png",
+								subtitle : "100% Cotton, 200% Comfortable",
+								default_action : {
+									type : "web_url",
+									url : "https://peterssendreceiveapp.ngrok.io/view?item=100",
+									messenger_extensions : true,
+									webview_height_ratio : "tall",
+									fallback_url : "https://peterssendreceiveapp.ngrok.io/"
+								},
+								buttons : [ {
+									title : "Shop Now",
+									type : "web_url",
+									url : "https://peterssendreceiveapp.ngrok.io/shop?item=100",
+									messenger_extensions : true,
+									webview_height_ratio : "tall",
+									fallback_url : "https://peterssendreceiveapp.ngrok.io/"
+								} ]
+							} ]
+				}
+			}
+		}
+	};  
 	  
 	  
 	  
-//	  payload: {
-//          template_type: "button",
-//          text: "Conta Fácil",
-//          buttons:[{
-//            type: "web_url",
-//            url: "http://www.globo.com",
-//            title: "Abrir Conta Fácil"
-//          }]
-//        }
+// payload: {
+// template_type: "button",
+// text: "Conta Fácil",
+// buttons:[{
+// type: "web_url",
+// url: "http://www.globo.com",
+// title: "Abrir Conta Fácil"
+// }]
+// }
 
 	  callSendAPI(messageData);
 	}
 
 /*
  * Send a Structured Message (Generic Message type) using the Send API.
- *
+ * 
  */
 function sendGenericMessage(recipientId) {
   var messageData = {
